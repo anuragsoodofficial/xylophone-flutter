c94c39adaf17dedfa30ce9c8c3cc4f2d1b177462
Anurag Sood <anurag.sood007@gmail.com>
2019-07-03 14:04:54 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,23 +3,23 @@ import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
-void playSound(int soundNumber) {
-  final player = AudioCache();
-  player.play('note$soundNumber.wav');
-}
-
-Expanded buildKey({Color color, int soundNumber}) {
-  return Expanded(
-    child: FlatButton(
-      color: color,
-      onPressed: () {
-        playSound(soundNumber);
-      },
-    ),
-  );
-}
-
 class XylophoneApp extends StatelessWidget {
+  void playSound(int key) {
+    final player = AudioCache();
+    player.play('note$key.wav');
+  }
+
+  Expanded generateKey(int keyNum, Color keyColor) {
+    return Expanded(
+      child: FlatButton(
+        onPressed: () {
+          playSound(keyNum);
+        },
+        color: keyColor,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -29,13 +29,12 @@ class XylophoneApp extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              buildKey(color: Colors.red, soundNumber: 1),
-              buildKey(color: Colors.orange, soundNumber: 2),
-              buildKey(color: Colors.yellow, soundNumber: 3),
-              buildKey(color: Colors.green, soundNumber: 4),
-              buildKey(color: Colors.teal, soundNumber: 5),
-              buildKey(color: Colors.blue, soundNumber: 6),
-              buildKey(color: Colors.purple, soundNumber: 7),
+              generateKey(1, Colors.red),
+              generateKey(2, Colors.orange),
+              generateKey(3, Colors.green),
+              generateKey(4, Colors.blue),
+              generateKey(5, Colors.purple),
+              generateKey(6, Colors.yellow),
             ],
           ),
         ),
